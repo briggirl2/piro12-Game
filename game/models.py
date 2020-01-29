89be282036802103a2baceb2df82ba59de976ab8
briggirl2 <briggirl2@gmail.com>
2020-01-29 20:26:30 +0900

    css little...

--- game/models.py
+++ game/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+    INPUT_CHOICES = (
+        ('가위', '가위'),
+        ('바위', '바위'),
+        ('보', '보')
+    )
+
+    user1 = models.CharField(max_length=50)
+    user2 = models.CharField(max_length=50)
+    user1_input = models.CharField(max_length=10, choices=INPUT_CHOICES)
+    user2_input = models.CharField(max_length=10, choices=INPUT_CHOICES)
+    result = models.CharField(max_length=30)
--- game/views.py
+++ game/views.py
@@ -20,6 +20,8 @@ def playgame_computer(request):
         game_result_str = ['무승부', '승', '패'][game_result]
         print(game_result_str)
         context = {
+            'user1': '당신',
+            'user2': '컴퓨터',
             'user1_input': user1_input,
             'user2_input': user2_input,
             'game_result': game_result_str,
@@ -49,3 +51,15 @@ def makeresult(a, b):
 
 def select_login(request):
     return render(request, 'game/select_login.html')
+
+
+def gamelist(request):
+    # record = Record.objects.all()
+    # return render(request, 'game/list.html', {'record:': record, })
+    # 전적리스트
+    return render(request, 'game/list.html')
+
+def gamedetail(request, pk):
+    record = Record.objects.get(id=pk)
+    return render(request, 'game/detail.html', {'record:': record, })
+    # 전적보기
