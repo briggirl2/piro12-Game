d4ef0e88bdab72160b7eb19aaf077864a9fde27e
briggirl2 <briggirl2@gmail.com>
2020-01-30 15:05:28 +0900

    look good..

--- game/views.py
+++ game/views.py
@@ -1,7 +1,6 @@
 import random
 
 from django.contrib.auth import get_user_model
-from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 
